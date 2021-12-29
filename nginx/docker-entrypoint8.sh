72a04b60c286b12b758e56cc6fea98a6f715c6cb
Naba Das <nabad600@gmail.com>
2021-12-29 15:59:53 +0530

    Branch update

--- nginx/docker-entrypoint8.sh
+++ nginx/docker-entrypoint8.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
-if [ -f "/var/www/composer.json" ]
+if [[ ! -e /var/www/index.php ]] || [[ ! -e wp-includes/version.php ]] ; 
 then
-    cd /var/www/
-    composer install
-    echo "Dependencies updated"
-elif [ "$(ls -A "/var/www/")" ]; 
-then
-    echo "Directory is not Empty, Please deleted hiden file and directory"
-elif [ {LARAVEL_INSTALL} = false ];
-then   
-    composer create-project --prefer-dist laravel/laravel:^8.0 .
-fi
-if [ ! -f ".env" ]; 
-then
-    echo ".env file not found"
-    cp .env.example .env
+    echo "WordPress not found in /var/www/ - copying now..."
+    rsync -aP -f'+ /*' -f'+ *' /tmp/latest/wordpress/* /var/www/
 else
-    echo ".env file exit"
+    echo "WordPress already install..."
 fi
-echo "Application key set ...."
-php artisan key:generate
-chmod -R 777 /var/www/storage
 cp /app/default.conf /etc/nginx/conf.d/default.conf
 nginx -s reload
 rm -rf /var/preview
