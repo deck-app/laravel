3ca374b298dce04a05d1a9f63407921a8b0da174
nabad600 <nabad600@gmail.com>
2023-09-21 22:27:11 +0530

    xdebug bux fix

--- laravel/nginx/package.sh
+++ laravel/nginx/package.sh
@@ -4,4 +4,4 @@ for (( n=1; n<=$word; n++ ))
 do
 SERVER_ARRAY=`echo ${INSTALL_ADDITIONAL_EXTENSIONS} | awk '{print $'$n'}' | sed 's/.*-//'`
 sudo apk add --no-cache php${PHP_VERSION}-`echo ${SERVER_ARRAY}`
-done
+done``
